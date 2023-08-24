79788cc00c04366ccb05e81558b7155051addd67
Himanshu-Dangwal <himanshudangwal99@gmail.com>
2023-08-24 20:14:06 +0530

    Reading and Writing files Sync and Async in Nodejs

---
+++ 1-node-farm/starter/index.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+
+// const readData = fs.readFileSync("./txt/read-this.txt",'utf-8');
+
+// console.log(readData);
+
+// fs.writeFileSync("./txt/input.txt",`${readData}`);
