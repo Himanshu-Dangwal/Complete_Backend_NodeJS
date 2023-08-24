d261e9999009b0008181495f77e4949ad022ed98
Himanshu-Dangwal <himanshudangwal99@gmail.com>
2023-08-24 22:06:05 +0530

    http server creation

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -10,6 +10,11 @@ const fs = require("fs");
 
 const http = require("http");
 
-http.createServer((req,res) => {
+const server = http.createServer((req,res) => {
+    console.log(req);
     res.end("Hey from the server");
+})
+
+server.listen(8000,'127.0.0.1', () => {
+    console.log("Started listening on port 8000");
 })
