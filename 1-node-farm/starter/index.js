288613fcc8c69bf5b82a3f9d28f05594d6c21766
Himanshu-Dangwal <himanshudangwal99@gmail.com>
2023-08-25 10:37:39 +0530

    Completed the first Node Farm Basics

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,36 +1,96 @@
-const fs = require("fs");
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+const slugify = require('slugify');
+const replaceTemplate = require('./modules/replaceTemplates');
 
-// const readData = fs.readFileSync("./txt/read-this.txt",'utf-8');
+/////////////////////////////////
+// FILES
 
-// console.log(readData);
+// Blocking, synchronous way
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+// console.log(textIn);
+// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync('./txt/output.txt', textOut);
+// console.log('File written!');
 
-// fs.writeFileSync("./txt/input.txt",`${readData}`);
+// Non-blocking, asynchronous way
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//   if (err) return console.log('ERROR! 💥');
 
-//Server
+//   fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//     console.log(data2);
+//     fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+//       console.log(data3);
 
-const http = require("http");
-const path = require("path");
-const url  = require("url")
+//       fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+//         console.log('Your file has been written 😁');
+//       })
+//     });
+//   });
+// });
+// console.log('Will read file!');
 
-const server = http.createServer((req,res) => {
-    // console.log(req);
+/////////////////////////////////
+// SERVER
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  'utf-8'
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  'utf-8'
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  'utf-8'
+);
 
-    const pathName = req.url;
-    if(pathName === '/' || pathName === '/overview'){
-        res.end("This is overview");
-    }else if(pathName === '/product'){
-        res.end("This is product route")
-    }else{
-        res.writeHead(404,{
-            'Content-type' : 'text/html',
-            'my-header' : "This route doesn't exists"
-        })
-        res.end("Page not found")
-    }
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
 
-    // res.end("Hey from the server");
-})
+const slugs = dataObj.map(el => slugify(el.productName, { lower: true }));
+console.log(slugs);
 
-server.listen(8000,'127.0.0.1', () => {
-    console.log("Started listening on port 8000");
-})
+const server = http.createServer((req, res) => {
+  const { query, pathname } = url.parse(req.url, true);
+
+  // Overview page
+  if (pathname === '/' || pathname === '/overview') {
+    res.writeHead(200, {
+      'Content-type': 'text/html'
+    });
+
+    const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+    res.end(output);
+
+    // Product page
+  } else if (pathname === '/product') {
+    res.writeHead(200, {
+      'Content-type': 'text/html'
+    });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
+
+    // API
+  } else if (pathname === '/api') {
+    res.writeHead(200, {
+      'Content-type': 'application/json'
+    });
+    res.end(data);
+
+    // Not found
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'hello-world'
+    });
+    res.end('<h1>Page not found!</h1>');
+  }
+});
+
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening to requests on port 8000');
+});
